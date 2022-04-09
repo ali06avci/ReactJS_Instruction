a38e627c2eaa5b707f0c886dba52c03d94d5495c
Ali AVCI <ali06avci@gmail.com>
2022-04-09 13:05:41 +0300

    to-do-project added

---
+++ to-do-project/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>Azra's ToDo List</h1>
+      
+      </header>
+      
+
+      
+    </div>
+  );
+}
+
+export default App;
